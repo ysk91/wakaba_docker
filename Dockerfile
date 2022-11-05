1c3b76687f8d2ee7f5593cfc90eec08b0e2636cd
ysk91 <ysk91.engineer@gmail.com>
2022-11-05 16:49:18 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM php:7.0-apache
+RUN apt-get update
+RUN apt-get install -y vim
---
+++ sample.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>今日は何日？</title>
+</head>
+<body>
+<h1>今日は何日？</h1>
+<p>
+    今日の日付は
+    <?php
+    echo date('Y年m月d日');
+    ?>
+    です。
+</p>
+</body>
+</html>
